e6f9f86ce7316297c91e318b669ab51f6269e9ee
Daniel Rodrigues <daniel.rodrigues.98@hotmail.com>
2023-08-16 20:00:14 -0300

    return json

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 )
 
 func HelloHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello World!")
+	return c.JSON(fiber.Map{"message": fmt.Sprintf("Hello, %s", c.Params("name"))})
 }
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", HelloHandler)
+	app.Get("/:name", HelloHandler)
 
 	app.Listen(":3000")
+
 }
