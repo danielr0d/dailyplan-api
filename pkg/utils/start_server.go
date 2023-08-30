e797e398328b31be7e1a80e01e26deaf4d85f706
Daniel Rodrigues <daniel.rodrigues.98@hotmail.com>
2023-08-29 23:12:02 -0300

    add start server and validator

---
+++ pkg/utils/start_server.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func StartServerWithGracefulShutdown(a *fiber.app) {
+	//Create channel for idle connections.
+	idleConnsClosed := make(chan struct{})
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt) //Catch OS signals
+		<-sigint
+
+		if err := a.Shutdown(); err != nil {
+			log.Printf("Oops... Server is not shutting down! Reason: %v", err)
+		}
+
+		close(idleConnsClosed)
+	}()
+
+	// Build Fiber connection URL.
+	fiberConnURL, _ := ConnectionURLBuilder("fiber")
+
+	// Run server.
+	if err := a.Listen(fiberConnURL); err != nil {
+		log.Printf("Oops... Server is not running! Reason: %v", err)
+	}
+
+	<-idleConnsClosed
+}
+
+// StartServer func for starting a simple server.
+func StartServer(a *fiber.App) {
+	// Build Fiber connection URL.
+	fiberConnURL, _ := ConnectionURLBuilder("fiber")
+
+	// Run server.
+	if err := a.Listen(fiberConnURL); err != nil {
+		log.Printf("Oops... Server is not running! Reason: %v", err)
+	}
+}
